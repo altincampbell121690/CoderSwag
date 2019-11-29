e558d654fc4bcfbc5f740064bde96708b7b933a3
altin <altincampbell121690@gmail.com>
2019-11-29 15:07:29 -0800

    recyclerAdapter refactor

---
+++ app/src/main/java/com/example/coderswag/Adapters/CategoryRecycleAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.coderswag.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.coderswag.Model.Category
+import com.example.coderswag.R
+import kotlinx.android.synthetic.main.category_list_item.view.*
+
+// recyclerviews need a <view holder?>
+                                                // we need the categories to show <- needs list type
+class CategoryRecycleAdapter(val context:Context, val categories:List<Category>) : RecyclerView.Adapter<CategoryRecycleAdapter.RvHolder>() {
+
+    //holder view class needed to hold the view and enable reuse of views (holds a refrence to the views)
+    // also resposible for binding the views
+    inner class RvHolder(itemView: View) : RecyclerView.ViewHolder(itemView) { // so we create one <- view holder
+
+        // in charge of binding? so lets bind these values (we need image and name here)
+        val categoryImage = itemView.findViewById<ImageView>(R.id.categoryImageImgVw) //grab imgVw ref
+        val categoryName = itemView.findViewById<TextView>(R.id.categoryNameTxtVw)//grab textVw ref
+
+
+        // function to pass in the category to bind it to to the ui elements
+        fun bindCategory(category:Category, context: Context){ // need context to find resource based on the name
+            val resourceId = context.resources.getIdentifier(category.image, "drawable",context.packageName)
+            categoryImage.setImageResource(resourceId)
+            categoryName.text = category.title
+        }
+    }
+
+
+    // this function is called by the recycler to display the data at a specefied location
+    // sp ot takes the viewholder and position and calls inner function bindCategory
+//    and passes in the categories list
+    override fun onBindViewHolder(myViewHolder: RvHolder, position: Int) {
+        myViewHolder.bindCategory(categories[position], context)
+
+    }
+
+    // method called when new view holders are needed
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvHolder {
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.category_list_item, parent, false
+        )
+        return RvHolder(view)
+
+    }
+
+
+    override fun getItemCount(): Int {
+        return categories.count() // number of items in the list
+    }
+
+
+
+}
--- app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.provider.ContactsContract
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.coderswag.Adapters.CategoryAdapter
+import com.example.coderswag.Adapters.CategoryRecycleAdapter
 import com.example.coderswag.Model.Category
 import com.example.coderswag.R
 import com.example.coderswag.Services.DataServices
@@ -15,7 +17,9 @@ class MainActivity : AppCompatActivity() {
 // middle man between the data and the view
 
     //lateinit var myAdapter:ArrayAdapter<Category> // default adapter
-    lateinit var myAdapter: CategoryAdapter // custom adapter
+    //lateinit var myAdapter: CategoryAdapter // custom adapter
+
+    lateinit var  myAdapter : CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,21 +31,37 @@ class MainActivity : AppCompatActivity() {
                 2 the type of view
                 3 needs to know the type of data
          * */
+    /* LIST VIEW
 
        // myAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataServices.categories)  //default view
         // need to pass in the categories
 
-        myAdapter = CategoryAdapter(this, DataServices.categories)
+        //myAdapter = CategoryAdapter(this, DataServices.categories) // for listview
+
 
         // now tell the list view who it needs to listen too
         // to set the adapter
-        categoryListView.adapter = myAdapter
+       // categoryListView.adapter = myAdapter
 
 /*       this is how we set item click functionality */
        /* categoryListView.setOnItemClickListener { parent, view, position, id ->
             val category = DataServices.categories[position]
             Toast.makeText(this, "you clicked ${category.title}", Toast.LENGTH_SHORT).show()
         }*/
+*/
+
+        ///RECYCLE
+        myAdapter = CategoryRecycleAdapter(this,DataServices.categories) // for recycler view
+
+        // now tell the list view who it needs to listen too
+        // to set the adapter
+        categoryListView.adapter = myAdapter
+        //we also need a layout manager to postions item views inside a recycler view
+        //determins when to reuse  or recylce a view and determines which type of layout
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
 
     }
 }
